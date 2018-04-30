bbf8a9ea2056e7666bbf3390390ae1da4c5a0f11
zhangweipeng <zhangweipengphp@163.com>
2018-05-01 01:02:33 +0800

    添加火币api调用

--- app/Http/Controllers/HuobiStrategyController.php
+++ app/Http/Controllers/HuobiStrategyController.php
@@ -3,13 +3,54 @@
 namespace App\Http\Controllers;
 
 use App\Services\TradePlatform;
+use Khill\Lavacharts\Lavacharts;
 
 class HuobiStrategyController extends Controller
 {
      public function getHuobiDepth()
      {
           $huoBi = app('HuoBi');
-          $symbols = $huoBi->get_common_symbols();
-          dd($symbols);
+          $depths = $huoBi->get_market_depth('btcusdt', 'step0');
+          $bids = $depths->tick->bids;
+          $asks = $depths->tick->asks;
+
+          //buy
+          $lava = new Lavacharts; // See note below for Laravel
+          $stocksTableBuy = $lava->DataTable();  // Lava::DataTable() if using Laravel
+          $stocksTableBuy->addNumberColumn('Price')
+               ->addNumberColumn('Amount');
+          foreach ($bids as $k => $b) {
+               $stocksTableBuy->addRow([
+                    $bids[$k][0], $bids[$k][1]
+               ]);
+          }
+          $lava->ColumnChart('Finances', $stocksTableBuy, [
+               'title' => 'market depth (bids)',
+               'titleTextStyle' => [
+                    'color'    => '#eb6b2c',
+                    'fontSize' => 14
+               ],
+          ]);
+          //ask
+          $lava2 = new Lavacharts; // See note below for Laravel
+          $stocksTableAsk = $lava2->DataTable();  // Lava::DataTable() if using Laravel
+          $stocksTableAsk->addNumberColumn('Price')
+               ->addNumberColumn('Amount');
+
+          foreach ($asks as $k => $a) {
+               $stocksTableAsk->addRow([
+                    $asks[$k][0], $bids[$k][1]
+               ]);
+          }
+
+          $lava2->ColumnChart('Finances', $stocksTableAsk, [
+               'title' => 'market depth (asks)',
+               'titleTextStyle' => [
+                    'color'    => '#eb6b2c',
+                    'fontSize' => 14
+               ],
+          ]);
+
+          return view('depth', ['lava' => $lava, 'lava2' => $lava2]);
      }
 }
---
+++ resources/views/depth.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.layout')
+
+@section('main_content')
+
+    <!-- Content Header (Page header) -->
+    {{--<section class="content-header">--}}
+        {{--<h1>--}}
+            {{--交易对--}}
+        {{--</h1>--}}
+        {{--<ol class="breadcrumb">--}}
+            {{--<li><a href="#"><i class="fa fa-dashboard"></i> 管理后台</a></li>--}}
+            {{--<li class="active">交易对</li>--}}
+        {{--</ol>--}}
+    {{--</section>--}}
+
+    <!-- Main content -->
+    <section class="content">
+    <!-- Your Page Content Here -->
+        <div class="row">
+            {{--<section class="col-lg-12 connectedSortable ui-sortable">--}}
+                    {{--<div class="box-header with-border">--}}
+                        {{--<h5 class="box-title">五日线法 测试日志</h5>--}}
+                    {{--</div>--}}
+                    {{--<div class="box-body">--}}
+                        {{--<div class="dataTables_wrapper form-inline dt-bootstrap">--}}
+
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="box-footer clearfix">--}}
+
+                    {{--</div>--}}
+                    <div id="pop_div" style="width:100%; height: 200px;"></div>
+                     <?= $lava->render('ColumnChart', 'Finances', 'pop_div') ?>
+                    <div id="pop_div2" style="width:100%; height: 200px;"></div>
+                     <?= $lava2->render('ColumnChart', 'Finances', 'pop_div2') ?>
+            {{--</section>--}}
+        </div>
+
+    </section>
+    <!-- /.content -->
+
+@endsection
