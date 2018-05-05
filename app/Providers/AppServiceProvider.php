3e7a8a98c75b31812d5160190f96dc9969a56bab
zhangweipeng <zhangweipengphp@163.com>
2018-05-05 23:44:39 +0800

    binance深度统计

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton("Binance", function(){
             $key = config('platform.binance.key');
             $secret = config('platform.binance.secret');
-            $api = new Binance\API($key,$secret);
+            $api = new Binance($key,$secret);
             return $api;
         });
         $this->app->singleton("HuoBi", function(){
